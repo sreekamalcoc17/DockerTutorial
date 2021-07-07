90beb9db99752852ca1ac4923aa7535e08fd6a3e
Sree Kamal <sreekamalcoc17@gmail.com>
2021-07-07 22:41:44 +0530

    tutorial

---
+++ src/test/java/com/jenkins/java/TestMain.java
@@ -0,0 +1,15 @@
+package com.jenkins.java;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestMain {
+    
+    @Test
+    public void testInputIsEven(){
+        System.out.println("Executing test");
+        assertTrue(Main.checkIfInputIsAnEvenNumber(22)); // Assertion
+    }
+}
